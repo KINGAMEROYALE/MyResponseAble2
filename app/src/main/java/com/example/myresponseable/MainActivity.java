f0e762e58db0ae019cb214325f4dc057fe07572b
KINGAMEROYALE <nirkrohmalik@gmail.com>
2024-02-02 19:24:07 +0200

    initial commit

--- app/src/main/java/com/example/myresponseable/MainActivity.java
+++ app/src/main/java/com/example/myresponseable/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText passwordET, emailET;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         passwordET = findViewById(R.id.password);
         emailET = findViewById(R.id.email);
+        auth = FirebaseAuth.getInstance();
+        //auth.createUserWithEmailAndPassword();
+        //auth.signInWithEmailAndPassword();
 
     }
 }
